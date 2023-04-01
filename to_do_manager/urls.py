a3166bf0bf5a0ea837d4f9e1eaa6d5c56f79109e
evgenijmartynuk07 <evgenijmartynuk07@gmail.com>
2023-04-01 17:51:18 +0300

    Change html files and views with urls

--- to_do_manager/urls.py
+++ to_do_manager/urls.py
@@ -1,13 +1,17 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from .views import TaskListView, TaskCreateView, TaskUpdateView
+from .views import TaskListView, TaskCreateView, TaskUpdateView, TaskDeleteView, TagListView, TagCreateView, TagUpdateView, TagDeleteView
 
 urlpatterns = [
     path("", TaskListView.as_view(), name="task-list"),
     path("task/create/", TaskCreateView.as_view(), name="task-create"),
     path("task/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
-    path("tag/", TaskListView.as_view(), name="tag-list"),
+    path("task/<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
+    path("tag/", TagListView.as_view(), name="tag-list"),
+    path("tag/create/", TagCreateView.as_view(), name="tag-create"),
+    path("tag/<int:pk>/update/", TagUpdateView.as_view(), name="tag-update"),
+    path("tag/<int:pk>/delete/", TagDeleteView.as_view(), name="tag-delete"),
 ]
 
 
--- to_do_manager/views.py
+++ to_do_manager/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views import generic
 
 from .models import Task, Tag
@@ -6,18 +7,20 @@ from .models import Task, Tag
 
 class TaskListView(generic.ListView):
     model = Task
-    queryset = Task.objects.prefetch_related("tag")
-    ordering = ["created", "is_completed"]
+    queryset = Task.objects.prefetch_related("tags")
+    ordering = ["-created", "is_completed"]
 
 
 class TaskCreateView(generic.CreateView):
     model = Task
     fields = "__all__"
+    success_url = reverse_lazy("to_do_list:task-list")
 
 
 class TaskUpdateView(generic.UpdateView):
     model = Task
     fields = "__all__"
+    reverse_lazy("to_do_list:task-list")
 
 
 class TaskDeleteView(generic.DeleteView):
@@ -26,3 +29,21 @@ class TaskDeleteView(generic.DeleteView):
 
 class TagListView(generic.ListView):
     model = Tag
+    queryset = Tag.objects.all()
+
+
+class TagCreateView(generic.CreateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("to_do_list:tag-list")
+
+
+class TagUpdateView(generic.UpdateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("to_do_list:tag-list")
+
+
+class TagDeleteView(generic.DeleteView):
+    model = Tag
+    success_url = reverse_lazy("to_do_list:tag-list")
